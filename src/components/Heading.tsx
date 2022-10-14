db81a625c866a0983f4b0146de9c12cc39899189
Igor <igor.asantosss@outlook.com>
2022-10-14 02:17:02 -0300

    Criação de página de Login

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,19 +6,29 @@ import { clsx } from "clsx";
 export interface HeadingProps {
   size?: "sm" | "md" | "lg";
   children: ReactNode;
-  asChild: boolean;
+  asChild?: boolean;
+  className?: string;
 }
 
-export const Heading = ({ size = "md", children, asChild }: HeadingProps) => {
+export const Heading = ({
+  size = "md",
+  children,
+  asChild,
+  className,
+}: HeadingProps) => {
   const WrapperTag = asChild ? Slot : "h2";
 
   return (
     <WrapperTag
-      className={clsx("text-gray-100 font-bold font-sans", {
-        "text-lg": size === "sm",
-        "text-xl": size === "lg",
-        "text-2xl": size === "md",
-      })}
+      className={clsx(
+        "text-gray-100 font-bold font-sans",
+        {
+          "text-lg": size === "sm",
+          "text-xl": size === "lg",
+          "text-2xl": size === "md",
+        },
+        className
+      )}
     >
       {children}
     </WrapperTag>
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -7,18 +7,28 @@ export interface TextProps {
   size?: "sm" | "md" | "lg";
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export const Text = ({ size = "md", children, asChild }: TextProps) => {
+export const Text = ({
+  size = "md",
+  children,
+  asChild,
+  className,
+}: TextProps) => {
   const WrapperTag = asChild ? Slot : "span";
 
   return (
     <WrapperTag
-      className={clsx("text-gray-100 font-sans", {
-        "text-xs": size === "sm",
-        "text-md": size === "lg",
-        "text-sm": size === "md",
-      })}
+      className={clsx(
+        "text-gray-100 font-sans",
+        {
+          "text-xs": size === "sm",
+          "text-md": size === "lg",
+          "text-sm": size === "md",
+        },
+        className
+      )}
     >
       {children}
     </WrapperTag>
