10d94609181606d47ae88130ac627eb4a2092db8
Igor <igor.asantosss@outlook.com>
2022-10-20 12:51:32 -0300

    Storybook: alterando staticDirs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,9 +14,7 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true,
-    "interactionsDebugger": true,
   },
-  "staticDirs": ["../public"],
   viteFinal: (config, { configType }) => {
     if(configType === 'PRODUCTION') {
       config.base === '/design-system-ig-lab/'
